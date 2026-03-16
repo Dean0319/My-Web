926c28877c859c8f8ab683815bd8f39ac24571a7
Jialiang Xie <jiyingliuxing@gmail.com>
2026-03-16 20:18:47 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  // GitHub Pages 部署需要设置 base（通常是 /<repo>/）。
-  // 本地开发保持 "./" 即可；CI/CD 可通过环境变量 BASE_URL 覆盖。
-  base: process.env.BASE_URL || "./",
-});
